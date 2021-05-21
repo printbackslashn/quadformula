3ce33acc6cb49454d8bbd1997817a9aa022277e4
Mark <markwilkinson@students.westonps.org>
2021-05-21 01:01:22 +0000

    Fixed somethings, made it neet, some things don't work maybe undefined

--- quad.c
+++ quad.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <cs50.h> //I will remove this later
 #include <math.h>
+#include <string.h>
 
+float quadFuncPos(float a, float b, float c);
+float quadFuncNeg(float a, float b, float c);
 int main(void)
 {
-    float a = get_int("Value of a: ");
-    float b = get_int("Value of b: ");
-    float c = get_int("Value of c: ");
-    float topPos = -b + sqrt((b * b) - (4 * a * c));
-    float topNeg = -b - sqrt((b * b) - (4 * a * c));
-    float neg = topNeg / (2 * a);
-    float pos = topPos / (2 * a);
-    if (neg == pos)
-    {
-        printf("x is %f.\n", pos);
-    }
-    else
-    {
-        printf("x is %f, %f.\n", neg, pos);
-    }
+    float a = get_float("Value of a: ");
+    float b = get_float("Value of b: ");
+    float c = get_float("Value of c: ");
+    float neg = quadFuncNeg(a, b, c);
+    float pos = quadFuncPos(a, b, c);
+    printf("x is %f, %f.\n", quadFuncNeg(a, b, c), quadFuncPos(a, b, c));
 
     //Works with two solution with a:3 b:6 c:-8
+}
+float quadFuncPos(float a, float b, float c)
+{
+
+    float topPos = -b + sqrt((b * b) - (4 * a * c));
+    float pos = topPos / (2 * a);
+    return pos;
+
+
+}
+float quadFuncNeg(float a, float b, float c)
+{
+    float topNeg = -b - sqrt((b * b) - (4 * a * c));
+    float neg = topNeg / (2 * a);
+    return (topNeg / (2 * a));
 }
