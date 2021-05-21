f78d98912e54daa14fdc6e83bfd0f1b99574031b
Mark <markwilkinson@students.westonps.org>
2021-05-21 00:39:58 +0000

    Turns out it was broken, also last known config reset

--- quad.c
+++ quad.c
@@ -7,10 +7,18 @@ int main(void)
     float a = get_int("Value of a: ");
     float b = get_int("Value of b: ");
     float c = get_int("Value of c: ");
-    float topPos = -5 + sqrt((b * b) - (4 * a * c));
-    float topNeg = -5 - sqrt((b * b) - (4 * a * c));
+    float topPos = -b + sqrt((b * b) - (4 * a * c));
+    float topNeg = -b - sqrt((b * b) - (4 * a * c));
     float neg = topNeg / (2 * a);
     float pos = topPos / (2 * a);
-    printf("x is %f, %f.\n", neg, pos); // Not sure how it works with one solution
+    if (neg == pos)
+    {
+        printf("x is %f.\n", pos);
+    }
+    else
+    {
+        printf("x is %f, %f.\n", neg, pos);
+    }
+
     //Works with two solution with a:3 b:6 c:-8
 }
